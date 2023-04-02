15edd8c8c73dc1db6b93e537d8f20632822d1579
주냉 <124892453+Jun-Haeng@users.noreply.github.com>
2023-04-02 14:05:45 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 220 ms, Memory: 18048 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.printf("%.0f", 4 * in.nextDouble());
+    }
+}
