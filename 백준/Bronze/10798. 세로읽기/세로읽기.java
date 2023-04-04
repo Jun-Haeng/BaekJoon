8923fa2924b755dc8b29dc94b7040e1dade5dff1
주냉 <124892453+Jun-Haeng@users.noreply.github.com>
2023-04-04 15:00:19 +0900

    [Bronze I] Title: 세로읽기, Time: 216 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        char[][] arr = new char[5][15];
+        int max = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            String str = in.nextLine();
+            if (max < str.length()) max = str.length();
+
+            for (int j = 0; j < str.length(); j++) {
+                arr[i][j] = str.charAt(j);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < max; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (arr[j][i] == '\0') continue;
+                sb.append(arr[j][i]);
+            }
+        }
+
+        System.out.println(sb);
+
+        in.close();
+    }
+}
