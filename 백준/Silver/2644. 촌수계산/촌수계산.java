5300d446b88f1cf27fef8e96a038f41b539858fd
주냉 <124892453+Jun-Haeng@users.noreply.github.com>
2023-04-07 19:45:38 +0900

    [Silver II] Title: 촌수계산, Time: 120 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -1,52 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int end, answer = -1;
+
+    static int n, m;
+    static int start, target;
     static boolean[] visited;
-    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static int[][] arr;
+    static boolean flag;
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        visited = new boolean[n + 1];
-        for (int i = 0; i <= n; i++) graph.add(new ArrayList<>());
+        n = Integer.parseInt(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int start = Integer.parseInt(st.nextToken());
-        end = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        start = Integer.parseInt(st.nextToken());
+        target = Integer.parseInt(st.nextToken());
 
-        int m = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
+        arr = new int[n+1][n+1];
+        visited = new boolean[n+1];
+
         for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            graph.get(from).add(to);
-            graph.get(to).add(from);
+            arr[a][b] = arr[b][a] = 1;
         }
 
+        flag = false;
         dfs(start, 0);
-        System.out.println(answer);
+        if (!flag) System.out.println(-1);
     }
 
-    private static void dfs(int start, int cnt) {
+    private static void dfs(int start, int depth) {
+
+        if (start == target) {
+            flag = true;
+            System.out.println(depth);
+            return;
+        }
+
         visited[start] = true;
 
-        for (Integer x : graph.get(start)) {
-            if (!visited[x]) {
-                if (x == end) {
-                    answer = cnt + 1;
-                    return;
-                }
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i] && arr[start][i] == 1) {
 
-                dfs(x, cnt + 1);
+                dfs(i, depth + 1);
+                visited[i] = true;
             }
-
         }
     }
 }
