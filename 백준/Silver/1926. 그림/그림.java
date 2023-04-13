4167e43b00e944b548d6e5425310292b9134de36
주냉 <124892453+Jun-Haeng@users.noreply.github.com>
2023-04-13 12:28:42 +0900

    [Silver I] Title: 그림, Time: 472 ms, Memory: 43800 KB -BaekjoonHub

---
+++ 백준/Silver/1926. 그림/그림.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static Queue<int[]> q = new LinkedList<>();
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int[][] map;
+    static int res;
+    static int max = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int cnt = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 1) {
+                    max = Math.max(bfs(i, j), max);
+                    cnt++;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+        System.out.println(max);
+    }
+
+    public static int bfs(int a, int b) {
+
+        q.add(new int[] {a, b});
+        res = 0;
+
+        while (!q.isEmpty()) {
+            int[] tmp = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = tmp[0] + dx[i];
+                int ny = tmp[1] + dy[i];
+
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                if (map[nx][ny] == 0) continue;
+
+                q.add(new int[] {nx, ny});
+                map[nx][ny] = 0;
+                res++;
+            }
+        }
+
+        if (res == 0) return 1;
+        return res;
+    }
+}
