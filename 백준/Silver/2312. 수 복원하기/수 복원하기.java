eabdb790a28978fd7eb90fd492a95e50f8dc904c
주냉 <124892453+Jun-Haeng@users.noreply.github.com>
2023-04-23 19:41:26 +0900

    [Silver III] Title: 수 복원하기, Time: 308 ms, Memory: 22736 KB -BaekjoonHub

---
+++ 백준/Silver/2312. 수 복원하기/수 복원하기.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int T = in.nextInt();
+        StringBuilder sb = new StringBuilder();
+
+        for (int t = 0; t < T; t++) {
+            int N = in.nextInt();
+
+            HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+            int tmp = 2;
+            while (true) {
+
+                if (N == 1) break;
+
+                if (N % tmp == 0) {
+                    N = N / tmp;
+                    hashMap.put(tmp, hashMap.getOrDefault(tmp, 0) + 1);
+                } else {
+                    tmp++;
+                }
+            }
+
+            Map<Integer, Integer> treeMap = new TreeMap<>(hashMap);
+
+            for (Map.Entry<Integer, Integer> entry : treeMap.entrySet()) {
+                sb.append(entry.getKey()).append(" ").append(entry.getValue()).append('\n');
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
