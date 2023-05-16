7dc8ebc770c77da4406b249dd7dad0941f6f46e8
주냉 <124892453+Jun-Haeng@users.noreply.github.com>
2023-05-16 20:35:09 +0900

    [Silver III] Title: N과 M (8), Time: 176 ms, Memory: 17664 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -2,50 +2,55 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N;
-    static int M;
+    static int N, M;
+    static int[] ans;
     static int[] arr;
-    static int[] memory;
+    static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
-    static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine(), " ");
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        arr = new int[M];
-        memory = new int[N];
+        ans = new int[M];
+        arr = new int[N];
+        visited = new boolean[N];
 
         st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
-            memory[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(memory);
+        Arrays.sort(arr);
 
         dfs(0, 0);
         System.out.println(sb);
+
     }
 
     static void dfs(int at, int depth) {
-        if (M == depth) {
-            for (int val : arr) {
-                sb.append(val).append(' ');
+        if (depth == M) {
+
+            for (int i : ans) {
+                sb.append(i).append(' ');
             }
-            sb.append("\n");
+            sb.append('\n');
             return;
         }
 
         for (int i = at; i < N; i++) {
-            arr[depth] = memory[i];
-            dfs(i,depth + 1);
+
+            ans[depth] = arr[i];
+            dfs(i, depth + 1);
         }
     }
 }
