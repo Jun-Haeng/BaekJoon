a805383e59c9d1f71e2f1f147479cf4bfc7311aa
주냉 <124892453+Jun-Haeng@users.noreply.github.com>
2023-05-23 17:03:10 +0900

    [Gold IV] Title: 알파벳, Time: 1024 ms, Memory: 15128 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,74 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int R, C;
-    static char[][] map;
-    static boolean[][] v;
-    static boolean[] arr = new boolean[26];
+    static int[][] map;
+    static boolean[] visited = new boolean[26];
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static int max = 0;
-    static Deque<int[]> q = new LinkedList<>();
+    static int ans = 0;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-
-        map = new char[R][C];
-        v = new boolean[R][C];
+        map = new int[R][C];
 
         for (int i = 0; i < R; i++) {
             String tmp = br.readLine();
 
             for (int j = 0; j < C; j++) {
-                map[i][j] = tmp.charAt(j);
+                map[i][j] = tmp.charAt(j) - 'A';
             }
         }
+        
+        backTracking(0, 0, 0);
 
-        backTracking(0, 0, 1);
-
-        System.out.println(max);
+        System.out.println(ans);
     }
 
     static void backTracking(int x, int y, int depth) {
-
-        q.add(new int[] {x, y});
-        v[x][y] = true;
-        int pos = map[x][y] - 'A';
-        arr[pos] = true;
-
-        while (!q.isEmpty()) {
-
-            int[] tmp = q.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int nx = tmp[0] + dx[i];
-                int ny = tmp[1] + dy[i];
-
-                if (nx < 0 || ny < 0 || nx >= R || ny >= C) continue;
-                if (v[nx][ny]) continue;
-                pos = map[nx][ny] - 'A';
-                if (arr[pos]) continue;
-
-                backTracking(nx, ny, depth + 1);
-
-                q.pollLast();
-                v[nx][ny] = false;
-                arr[pos] = false;
-            }
+        if (visited[map[x][y]]) {
+            ans = Math.max(ans, depth);
+            return;
         }
 
-        max = Math.max(depth, max);
+        visited[map[x][y]] = true;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || ny < 0 || nx >= R || ny >= C) continue;
+            backTracking(nx, ny, depth + 1);
+        }
+
+        visited[map[x][y]] = false;
     }
 }
