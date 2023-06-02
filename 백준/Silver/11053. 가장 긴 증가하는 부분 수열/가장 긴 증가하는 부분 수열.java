fef1279735416f7208ab1924987e094ea74f4e6c
주냉 <124892453+Jun-Haeng@users.noreply.github.com>
2023-06-02 21:09:27 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 144 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,51 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static Integer dp[];
-    static int arr[];
+    static int[] dp;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        dp = new Integer[n];
-        arr = new int[n];
+        dp = new int[N+1];
+        arr = new int[N+1];
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < n; i++) {
-            LIS(i);
+        for (int i = 1; i <= N; i++) {
+            int max = 0;
+
+            for (int j = 0; j <= i - 1; j++) {
+                if (arr[i] > arr[j]) {
+                    max = Math.max(max, dp[j]);
+                }
+
+                dp[i] = max + 1;
+            }
         }
 
-        int max = dp[0];
-
-        for (int i = 0; i < n; i++) {
-            if (dp[i] > max) max = dp[i];
+        int max = 0;
+        for (int i = 1; i <= N; i++) {
+            max = Math.max(max, dp[i]);
         }
 
         System.out.println(max);
 
-    }
-    static int LIS(int n) {
-        if (dp[n] == null) {
-            dp[n] = 1;
 
-            for (int i = n - 1; i >= 0; i--) {
-                if (arr[i] < arr[n]) {
-                    dp[n] = Math.max(dp[n], LIS(i) + 1);
-                }
-            }
-        }
-        return dp[n];
     }
 }
