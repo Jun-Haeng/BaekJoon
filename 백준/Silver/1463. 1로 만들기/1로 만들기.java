a77a6eeb0d3fdc230741067e1f170f23b731b82c
주냉 <124892453+Jun-Haeng@users.noreply.github.com>
2023-06-02 19:21:18 +0900

    [Silver III] Title: 1로 만들기, Time: 256 ms, Memory: 66004 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,37 +1,42 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    static int[] dp;
+    static Integer[] dp;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner in = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = in.nextInt();
-        dp = new int[N+1];
+        int N = Integer.parseInt(br.readLine());
+        dp = new Integer[N+1];
 
         System.out.println(solve(N));
     }
 
-    static int solve(int N) {
-
+    public static int solve(int N) {
+        
+        // 초기값 설정
         if (N == 1) return 0;
-
-        if (dp[N] == 0) {
+        
+        // 과정
+        if (dp[N] == null) {
 
             if (N % 6 == 0) {
-                dp[N] = Math.min(Math.min(solve(N / 3), solve(N / 2)),
-                                solve(N - 1)) + 1;
+                dp[N] = Math.min(Math.min(solve(N / 3), solve(N / 2)
+                ), solve(N - 1)) + 1;
 
             } else if (N % 3 == 0) {
                 dp[N] = Math.min(solve(N / 3), solve(N - 1)) + 1;
 
-            } else if (N % 2 == 0){
+            } else if (N % 2 == 0) {
                 dp[N] = Math.min(solve(N / 2), solve(N - 1)) + 1;
 
             } else {
                 dp[N] = solve(N - 1) + 1;
+
             }
         }
 
