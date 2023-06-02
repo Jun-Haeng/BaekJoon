1908a7a931d9920a337a1c17205c443012ec63e4
주냉 <124892453+Jun-Haeng@users.noreply.github.com>
2023-06-02 19:39:48 +0900

    [Silver I] Title: 포도주 시식, Time: 148 ms, Memory: 15568 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,39 +1,40 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    static Integer dp[];
-    static int arr[];
+    static int[] arr;
+    static Integer[] dp;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
 
-        int n = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        dp = new Integer[n + 1];
-        arr = new int[n + 1];
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N+1];
+        dp = new Integer[N+1];
 
-        for (int i = 1; i <= n; i++) {
-            arr[i] = sc.nextInt();
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
         dp[0] = arr[0];
         dp[1] = arr[1];
 
-        if (n >= 2) dp[2] = arr[2] + arr[1];
+        if (N >= 2) dp[2] = arr[1] + arr[2];
 
-        System.out.println(find(n));
-
+        System.out.println(solve(N));
     }
 
-    static int find(int n) {
+    public static int solve(int N) {
 
-        if (dp[n] == null) {
-            dp[n] = Math.max(
-                    Math.max(find(n - 2), find(n - 3) + arr[n - 1]) + arr[n]
-                    , find(n - 1));
+        if (dp[N] == null) {
+
+            dp[N] = Math.max(Math.max(solve(N-3) + arr[N-1], solve(N-2))
+                + arr[N], solve(N-1));
         }
 
-        return dp[n];
+        return dp[N];
     }
 }
