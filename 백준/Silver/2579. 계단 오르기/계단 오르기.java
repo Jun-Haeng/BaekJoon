2cb134f7326e73fb11b38aaad1532c16531cab83
주냉 <124892453+Jun-Haeng@users.noreply.github.com>
2023-06-02 20:13:55 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,20 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    static Integer dp[];
-    static int arr[];
-    public static void main(String[] args) {
+    static int[] arr;
+    static Integer[] dp;
 
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
 
-        int N = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        dp = new Integer[N + 1];
-        arr = new int[N + 1];
+        int N = Integer.parseInt(br.readLine());
+
+        arr = new int[N+1];
+        dp = new Integer[N+1];
 
         for (int i = 1; i <= N; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
         dp[0] = arr[0];
@@ -22,13 +25,13 @@ public class Main {
 
         if (N >= 2) dp[2] = arr[1] + arr[2];
 
-        System.out.println(find(N));
+        System.out.println(solve(N));
     }
 
-    static int find(int N) {
+    public static int solve(int N) {
 
         if (dp[N] == null) {
-            dp[N] = arr[N] + Math.max(find(N - 2), find(N- 3) + arr[N - 1]);
+            dp[N] = Math.max(solve(N - 3) + arr[N - 1], solve(N - 2)) + arr[N];
         }
 
         return dp[N];
