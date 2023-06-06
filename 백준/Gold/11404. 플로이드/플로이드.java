2866cfdbeceb4ae2bb9c62ad759bc7d8bef52264
주냉 <124892453+Jun-Haeng@users.noreply.github.com>
2023-06-06 09:58:32 +0900

    [Gold IV] Title: 플로이드, Time: 768 ms, Memory: 44656 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static int[][] dist;
+    static final int INF = 100000000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+        dist = new int[N+1][N+1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == j) continue;
+                else dist[i][j] = INF;
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            dist[u][v] = Math.min(dist[u][v], w);
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (dist[i][j] == INF) System.out.print(0 + " ");
+                else System.out.print(dist[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
