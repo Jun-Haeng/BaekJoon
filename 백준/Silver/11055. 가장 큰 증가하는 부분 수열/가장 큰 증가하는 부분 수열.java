fb527b2b2f5d96071b803fe62c8bd46cd6480a13
주냉 <124892453+Jun-Haeng@users.noreply.github.com>
2023-06-13 13:13:25 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 152 ms, Memory: 14532 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -11,23 +11,20 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N+1];
-        Integer[] dp = new Integer[N+1];
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        dp[0] = 0;
-        dp[1] = 1;
+        int[] dp = new int[N+1];
 
         int maxSum = 0;
 
         for (int i = 1; i <= N; i++) {
-            dp[i] = arr[i];
             int max = 0;
 
-            for (int j = 0; j <= i - 1; j++) {
+            for (int j = 1; j <= i - 1; j++) {
                 if (arr[j] < arr[i]) {
                     max = Math.max(dp[j], max);
                 }
