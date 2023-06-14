72fdc1e54a5e4dc6674fc292faa5d18a1dd6d036
주냉 <124892453+Jun-Haeng@users.noreply.github.com>
2023-06-14 20:36:23 +0900

    [Silver I] Title: 동물원, Time: 136 ms, Memory: 16848 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.java
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 
@@ -10,16 +9,21 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int[][] dp = new int[N+1][3];
+        int[][] dp = new int[N+1][2];
 
-        Arrays.fill(dp[1], 1);
+        dp[1][0] = 1;
+        dp[1][1] = 2;
 
         for (int i = 2; i <= N; i++) {
-            dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % 9901;
-            dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % 9901;
-            dp[i][2] = (dp[i-1][0] + dp[i-1][1]) % 9901;
+            dp[i][0] = (dp[i-1][0] + dp[i-1][1]) % 9901;
+            dp[i][1] = (dp[i-1][0] * 2 + dp[i-1][1]) % 9901;
         }
 
-        System.out.println((dp[N][0] + dp[N][1] + dp[N][2]) % 9901);
+        System.out.println((dp[N][0] + dp[N][1]) % 9901);
+
+//        for (int[] ints : dp) {
+//            System.out.print(ints[0] + " " + ints[1]);
+//            System.out.println();
+//        }
     }
 }
